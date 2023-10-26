aa6a71f95f5e786d07f86aa15a71d81d27209445
ehdiH <hmehdih@HmehdiH.lan>
2023-10-26 03:32:32 +0200

    bug with find_index

---
+++ generate_todays_rotor.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define ALPHABET "abcdefghijklmnopqrstuvwxyz"
+
+void shuffle(char *arr, int size){
+    srand((unsigned int) time(NULL));
+    size--; 
+    for(int i = 0; i<size; i++){
+        int randIndex = i + rand()% (size-i);
+        char tmp = arr[i];
+        arr[i] = arr[randIndex];
+        arr[randIndex] = tmp;
+    }
+    
+}
+
+
+int main()
+{
+    char rotor1[27];
+    char rotor2[27]; 
+    char rotor3[27]; 
+    
+    strcpy(rotor1, ALPHABET);
+    shuffle(rotor1, 27);
+
+    strcpy(rotor2, rotor1);
+    shuffle(rotor2, 27);
+    
+    strcpy(rotor3,rotor2);
+    shuffle(rotor3, 27);
+
+    int fd = open("rotor_state_today", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if(fd == -1)
+    {
+        perror("fd fialed");
+        exit(-1);
+    }
+    
+    char data[83];
+    strcpy(data, rotor1);
+    strcat(data, " ");
+    strcat(data, rotor2);
+    strcat(data, " ");
+    strcat(data, rotor3);
+
+    // Write the structure to the file
+    ssize_t bytes_written = write(fd, &data, 83);
+    if (bytes_written == -1) {
+        perror("write failed");
+        close(fd);
+        return 1;
+    }
+
+    close(fd);
+
+    return 0;
+}
